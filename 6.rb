9b7bb7c964fd0ecdb1d438f44d7e54ac111392f3
leven <leven_liu@jiliguala.com>
2020-04-02 14:01:16 +0800

    new ruby test

--- 6.rb
+++ 6.rb
@@ -1,20 +1,26 @@
-# 类方法
-class Fo
-    def self.bar
-        p "aa"
+module M
+    def self.included(other)
+        p "#{other} included #{self}"
     end
-
-    class << self
-        def bar2
-            p 'bb'
+    class Fo
+        def self.bar
+            p "aa"
+        end
+        class << self
+            def bar2
+                p 'bb'
+            end
         end
     end
+    def bar
+        p "00"
+    end
+    
+    def Fo.bar3
+        p 'cc'
+    end    
 end
 
-def Fo.bar3
-    p 'cc'
-end
-
-Fo.bar
-Fo.bar2
-Fo.bar3
+# M::Fo.bar
+# M::Fo.bar2
+# M::Fo.bar3
--- 7.rb
+++ 7.rb
@@ -1,10 +1,20 @@
 require './6'
 module M
-    extend ITest
+    def self.included(other)
+        p "hahah"
+    end
     def test_method
         puts 'this is test'
     end
+    def M.extended(other)
+        p "i am extended by #{other}"
+    end
 end
+
+def M.included(other)
+    p "i am included by #{other}"
+end
+
 class A
     extend M
 end
@@ -12,3 +22,6 @@ end
 class B
     include M
 end
+
+M::Fo.bar
+B.new.bar
